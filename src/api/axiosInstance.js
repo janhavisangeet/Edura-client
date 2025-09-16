0b4f770073c20f6404eb81cb2447e09aa06838da
janhavi <sangeetjanhavi@gmail.com>
2025-09-16 20:39:05 +0530

    adding url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+import dotenv from "dotenv";
+dotenv.config();
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  
+  baseURL: "https://edura-server.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
@@ -18,3 +20,24 @@ axiosInstance.interceptors.request.use(
 );
 
 export default axiosInstance;
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", 
+//   withCredentials: true, // only if you’re using cookies
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
+
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+
+//     return config;
+//   },
+//   (err) => Promise.reject(err)
+// );
+
+// export default axiosInstance;
